109c914653212363655eca8d021a91d8e143641c
lebrancconvas <lebrancconvas@gmail.com>
2026-04-11 07:57:49 +0700

    ✨ feat: update scraping code.

--- utils/scrape.py
+++ utils/scrape.py
@@ -6,20 +6,35 @@ BASE_PATH = "../data/base/markdown"
 
 def run():
   with sync_playwright() as p:
-    browser = p.chromium.launch(headless=False)
+    browser = p.chromium.launch(headless=True)
     context = browser.new_context()
-    page = context.new_page()
+    page = context.new_page()  
+    
+    print(f"[LOG] Begin to scrape data.")
 
     page.goto(f"{BASE_URL}")
 
     # Click More
-    MORE_BUTTON = page.get_by_text("ดูทั้งหมด")
-    page.click("body > div.mainPage > div.mainPageContent > div > div > div.overviewGroupList.overflow-hidden > div.overviewGroupListContent > div > div > div.contentListLoadMoreArea.dataListLoadAllArea > button")
+    MORE_BUTTON_SELECTOR = "body > div.mainPage > div.mainPageContent > div > div > div.overviewGroupList.overflow-hidden > div.overviewGroupListContent > div > div > div.contentListLoadMoreArea.dataListLoadAllArea > button"
+    page.click(MORE_BUTTON_SELECTOR)
+
+    page.wait_for_timeout(1500)
 
     # Get ID
-    ETHNIC_LIST = "ethnicList"
-    result = page.locator(ETHNIC_LIST).get_attribute("href") 
-    print(result)  
+    ETHNIC_LIST_SELECTOR = ".contentListItem > a"  
+    result = page.query_selector_all(ETHNIC_LIST_SELECTOR)
+    
+    # Save Data URL
+
+    ethnicity_data_url = []
+
+    for data in result:
+      url = data.get_attribute("href")
+      ethnicity_data_url.append(url)
+    
+    print(ethnicity_data_url)
+    print(f"Size of Data: {len(ethnicity_data_url)}")
+    return ethnicity_data_url
 
 if __name__ == "__main__":
   run()
